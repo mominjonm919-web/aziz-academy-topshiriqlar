d98551e1c7887921225aa417ad201d46afafecab
mominjonm919-web <mominjonm919@gmail.com>
2026-06-15 20:10:21 +0500

    Add AI task: Kiruvchi matnning qisqartmasini chiqarish

---
+++ ai_topshiriqlar/Kiruvchi_matnning_qisqartmasini_chiqarish.py
@@ -0,0 +1,8 @@
+# Kiruvchi matnning qisqartmasini chiqarish
+# Kurs: Dasturlash / IT
+# Mavzu: 2-mavzu: Birinchi dastur, print() va kommentlar
+# Ball: 30
+# Aziz Academy — AI Topshiriq
+
+matn = input()
+print(matn[:3])
