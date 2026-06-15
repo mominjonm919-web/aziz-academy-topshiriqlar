0d37b3b632f41743057118af6c2ad8bf89ff2834
mominjonm919-web <mominjonm919@gmail.com>
2026-06-15 20:30:30 +0500

    Add: 1) Ikki butun son: +, -, * [mominjonm919-web]

---
+++ lms_kod_topshiriqlari/Arifmetik_operatorlar__prioritet/1__Ikki_butun_son_____-___.py
@@ -0,0 +1,7 @@
+a, b = map(int, input().split())
+sum_result = a + b 
+sub_result = a - b 
+mul_result = a * b 
+print(f"Add: {sum_result}")
+print(f"Sub: {sub_result}")
+print(f"Mul: {mul_result}")
