d8828527cac3a521b1e6f2a3fda904db1c7203b9
mominjonm919-web <mominjonm919@gmail.com>
2026-06-18 09:18:10 +0500

    Add: 15) Menyu: yakuniy [mominjonm919-web]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/15__Menyu__yakuniy.py
@@ -0,0 +1,9 @@
+choise = input()
+if choise == "start":
+    print("Started")
+elif choise == "stop":
+    print("Stopped")
+elif choise == "pause":
+    print("Paused")
+else:
+        print("Unknown command")
