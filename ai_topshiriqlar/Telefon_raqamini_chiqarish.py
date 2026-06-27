6668b1ca5a7fe8438dff2bad527de66229b9ddd0
mominjonm919-web <mominjonm919@gmail.com>
2026-06-27 07:58:44 +0500

    Add AI task: Telefon raqamini chiqarish

---
+++ ai_topshiriqlar/Telefon_raqamini_chiqarish.py
@@ -0,0 +1,12 @@
+# Telefon raqamini chiqarish
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+name = input()
+familya = input()
+tel_raqam = input()
+print(name)
+print(familya)
+print(tel_raqam)
