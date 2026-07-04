662836856171b04fd2946b3a1a7a2f1e6e68ea7b
mominjonm919-web <mominjonm919@gmail.com>
2026-07-04 20:15:13 +0500

    Add: Profil kartasi [mominjonm919-web]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
@@ -1,6 +1,11 @@
-print("===PROFIL===")
-print("Ism: Aziz")
-print("Famillya: Karimov")
-print("Kurs: Python")
-print("Daraja: Boshlang'ich")
-print("Shahar: Toshkent")
+ism = "Aziz"
+familiya = "Karimov"
+kurs = "Python"
+daraja = "Boshlang'ich"
+shahar = "Toshkent"
+print("=== PROFIL ===")
+print("Ism:", ism)
+print("Familiya:", familiya)
+print("Kurs:", kurs)
+print("Daraja:", daraja)
+print("Shahar:", shahar)
