d88a0ac1ab431c3167a3be431675549554d489a5
mominjonm919-web <mominjonm919@gmail.com>
2026-08-20 08:59:36 +0500

    Add AI task: Maydon hisobi

---
+++ ai_topshiriqlar/Maydon_hisobi.py
@@ -0,0 +1,12 @@
+# Maydon hisobi
+# Kurs: Dasturlash / IT
+# Mavzu: Birinchi dastur ⭐ — print() va kommentlar
+# Ball: 30
+# Aziz Academy — AI Topshiriq
+
+a = int(input())
+b = int(input())
+perimetr = 2 * (a + b)
+yuza = a * b 
+print(perimetr)
+print(yuza)
